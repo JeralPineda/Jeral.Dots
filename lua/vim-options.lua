25a803a817d33f7f2bdc4a3b67a1ba4280ae7f6c
Jeral Pineda <jeral.cris@gmail.com>
2024-07-11 22:04:30 -0600

    fix: update options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,18 +1,40 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-vim.g.background = "light"
+vim.cmd("let g:netrw_liststyle = 3")
 
-vim.opt.swapfile = false
+local opt = vim.opt
 
--- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.wo.number = true
+opt.relativenumber = true
+opt.number = true
 
+-- tabs & identation
+opt.tabstop = 2 -- 2 spases for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line 
+
+opt.wrap = true
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assume you want case-sensitive
+
+opt.cursorline = true
+
+-- turn on termguicolor for tokyonight colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+opt.background = "dark" --colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" --show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizzontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
