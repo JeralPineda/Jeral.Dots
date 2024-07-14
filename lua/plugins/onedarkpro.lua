337258190ffaa446fbe18a39ccbd74f6117eff3a
Jeral Pineda <jeral.cris@gmail.com>
2024-07-13 23:24:06 -0600

    feat: add theme onedarkpro

--- lua/plugins/onedarkpro.lua
+++ lua/plugins/onedarkpro.lua
@@ -1,5 +1,12 @@
 return {
-  "olimorris/onedarkpro.nvim",
-  priority = 1000, -- Ensure it loads first
+	"olimorris/onedarkpro.nvim",
+	priority = 1000, -- Ensure it loads first
+	config = function()
+		require("onedarkpro").setup({
+			options = {
+				cursorline = true,
+			},
+		})
+		--  vim.cmd("colorscheme onedark_vivid")
+	end,
 }
-
