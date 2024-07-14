95a3ed1168ea853148ea43780ec741b93cfd1c40
Jeral Pineda <jeral.cris@gmail.com>
2024-07-14 03:24:46 -0600

    fix: update telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
+    tag = "0.1.8",
     dependencies = {
       "nvim-lua/plenary.nvim",
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
@@ -12,6 +12,7 @@ return {
     },
     config = function()
       local actions = require("telescope.actions")
+      local telescope = require("telescope")
 
       require("telescope").setup({
         defaults = {
@@ -20,6 +21,7 @@ return {
             i = {
               ["<C-k>"] = actions.move_selection_previous, -- move to prev result
               ["<C-j>"] = actions.move_selection_next, -- move to next result
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
             },
           },
         },
@@ -27,15 +29,34 @@ return {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
-        }
+        },
       })
 
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+      telescope.load_extension("fzf")
+
+      -- set keymaps
+      local keymap = vim.keymap -- for conciseness
+
+      keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+      keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+      keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+      keymap.set(
+        "n",
+        "<leader>fc",
+        "<cmd>Telescope grep_string<cr>",
+        { desc = "Find string under cursor in cwd" }
+      )
+      vim.keymap.set("n", "<leader>fb", "<cmd>Telescope oldfiles<cr>", {})
+      vim.keymap.set("n", "<leader>fp", "<cmd>Telescope resume<cr>", {})
+      vim.keymap.set("n", "<leader>fd", "<cmd>Telescope diagnostics<cr>", {})
+      vim.keymap.set("n", "<leader>ft", "<cmd>Telescope treesitter<cr>", {})
+
+      --local builtin = require("telescope.builtin")
+      --vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      --vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      --vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
 
       require("telescope").load_extension("ui-select")
-    end
+    end,
   },
 }
