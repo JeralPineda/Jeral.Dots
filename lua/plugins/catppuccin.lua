9605fe9f64d4b52626c86cb9e44bb127eb031ce6
Jeral Pineda <jeral.cris@gmail.com>
2024-07-14 03:24:27 -0600

    refactor: refactorizacion de catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,23 +1,21 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        color_overrides = {
-          all = {},
-          latte = {},
-          frappe = {},
-          macchiato = {},
-          mocha = {
-            --base = "#15141f",
-          },
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      color_overrides = {
+        all = {},
+        latte = {},
+        frappe = {},
+        macchiato = {},
+        mocha = {
+          --base = "#15141f",
         },
-      })
+      },
+    })
 
-      vim.cmd.colorscheme("catppuccin-mocha")
-    end,
-  },
+    vim.cmd.colorscheme("catppuccin-mocha")
+  end,
 }
