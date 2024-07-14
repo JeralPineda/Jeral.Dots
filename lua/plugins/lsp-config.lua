43575dae163cf67372fea948cd381ff3cb7b2492
Jeral Pineda <jeral.cris@gmail.com>
2024-07-14 03:23:49 -0600

    fix: add organize imports keymap

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -14,14 +14,14 @@ return {
 		},
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { 
-          "lua_ls",
-          "tsserver",
-          "cssls", 
-          "html",
-          "tailwindcss",
-          "emmet_ls",
-        },
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"cssls",
+					"html",
+					"tailwindcss",
+					"emmet_ls",
+				},
 			})
 		end,
 	},
@@ -32,9 +32,24 @@ return {
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
+
+			local function organize_imports()
+				local params = {
+					command = "_typescript.organizeImports",
+					arguments = { vim.api.nvim_buf_get_name(0) },
+				}
+				vim.lsp.buf.execute_command(params)
+			end
+
 			lspconfig.tsserver.setup({
 				capabilities = capabilities,
-      })
+				commands = {
+					OrganizeImports = {
+						organize_imports,
+						description = "Organize Imports",
+					},
+				},
+			})
 			lspconfig.solargraph.setup({
 				capabilities = capabilities,
 			})
@@ -44,21 +59,22 @@ return {
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
-	    lspconfig["cssls"].setup({
+			lspconfig["cssls"].setup({
 				capabilities = capabilities,
 			})
-      lspconfig["tailwindcss"].setup({
-        capabilities = capabilities,
-      })
-      lspconfig["emmet_ls"].setup({
-          capabilities = capabilities,
-          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-      })
- 
+			lspconfig["tailwindcss"].setup({
+				capabilities = capabilities,
+			})
+			lspconfig["emmet_ls"].setup({
+				capabilities = capabilities,
+				filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+			})
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>oi", ":OrganizeImports <CR>", {})
 		end,
 	},
 }
