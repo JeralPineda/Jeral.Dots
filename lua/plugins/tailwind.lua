f7aaaf303b527f8265f43da8a51cddac2944019b
Jeral Pineda <jeral.cris@gmail.com>
2024-07-22 18:08:48 -0600

    fix: add plugin tailwind-tools
    Add new plugins for tailwind

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -15,14 +15,31 @@ return {
 			},
 		},
 	},
-  {
-    "roobert/tailwindcss-colorizer-cmp.nvim",
-    -- optionally, override the default options:
-    config = function()
-      require("tailwindcss-colorizer-cmp").setup({
-        --color_square_width = 2,
-        rgba = true,
-      })
-   end
-  },
+  -- {
+  --   "roobert/tailwindcss-colorizer-cmp.nvim",
+  --   -- optionally, override the default options:
+  --   config = function()
+  --     require("tailwindcss-colorizer-cmp").setup({
+  --       --color_square_width = 2,
+  --       rgba = true,
+  --     })
+  --  end
+  -- },
+	{
+		"luckasRanarison/tailwind-tools.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = function()
+			require("tailwind-tools").setup({})
+
+			-- set keymaps
+      local keymap = vim.keymap -- for conciseness
+
+      keymap.set("n", "<leader>tb", "<cmd>TailwindConcealToggle<cr>", { desc = "Toggles conceal class" })
+
+      keymap.set("n", "<leader>tg", "<cmd>TailwindColorToggle<cr>", { desc = "Toggles color hints" })
+
+      keymap.set("n", "<leader>ts", "<cmd>TailwindSort<cr>", { desc = "Sorts all classes in the current buffer" })
+
+		end,
+	}
 }
