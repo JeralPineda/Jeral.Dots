0ac6f73d9a0758d1144b1f0894d482f81cb554f5
Jeral Pineda <jeral.cris@gmail.com>
2024-08-01 22:38:25 -0600

    fix: apply transparent background

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,6 +5,7 @@ return {
   priority = 1000,
   config = function()
     require("catppuccin").setup({
+      transparent_background = true,
       color_overrides = {
         all = {},
         latte = {},
