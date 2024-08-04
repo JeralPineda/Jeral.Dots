069590b55fa58aa8ecdbe45935cf10558ce1f5d6
Jeral Pineda <jeral.cris@gmail.com>
2024-08-04 11:54:20 -0600

    fix: configuration for show view hidden files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     local oil = require("oil")
     oil.setup({
-       float = {
+      float = {
         -- Padding around the floating window
         padding = 2,
         max_width = 150,
@@ -20,7 +20,34 @@ return {
           return conf
         end,
       },
+
+      -- Set to false to disable all of the above keymaps
+      use_default_keymaps = true,
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+        -- This function defines what is considered a "hidden" file
+        is_hidden_file = function(name, bufnr)
+          return vim.startswith(name, ".")
+        end,
+        -- This function defines what will never be shown, even when `show_hidden` is set
+        is_always_hidden = function(name, bufnr)
+          return false
+        end,
+        -- Sort file names in a more intuitive order for humans. Is less performant,
+        -- so you may want to set to false if you work with large directories.
+        natural_order = true,
+        -- Sort file and directory names case insensitive
+        case_insensitive = false,
+        sort = {
+          -- sort order can be "asc" or "desc"
+          -- see :help oil-columns to see which columns are sortable
+          { "type", "asc" },
+          { "name", "asc" },
+        },
+      },
     })
-    vim.keymap.set("n", "-", oil.toggle_float, {desc = "Open oil file system"})
+    vim.keymap.set("n", "-", oil.toggle_float, { desc = "Open oil file system" })
   end,
 }
+
